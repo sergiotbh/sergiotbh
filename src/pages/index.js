d52bf969f96b6e2c8be5030f6b8ee7787c2d4696
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-03-05 14:43:25 -0600

    fixed mobile styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,11 @@ import { StaticImage } from "gatsby-plugin-image";
 const IndexPage = () => {
   return (
     <Layout>
-      <section className="relative flex bg-black">
-        <section className="h-screen w-screen snap-y snap-mandatory overflow-scroll">
+      <section className="relative flex flex-col bg-black">
+        <section className="pt-12 md:p-0 h-screen w-screen md:snap-y md:snap-mandatory overflow-scroll">
           <IntroSection/>
         </section>
-        <div className="absolute bottom-5 left-10 overflow-hidden w-2/5">
+        <div className="fixed bg-black md:absolute p-2 shadow-2xl md:p-0  md:bottom-5 md:left-10 overflow-hidden md:w-2/5">
           <StaticImage
             src="../images/sergiotbhlogo.png"
           />
@@ -27,8 +27,8 @@ const IndexPage = () => {
 const IntroSection = () => {
   
   return(
-    <section className={`grid grid-cols-5 h-screen bg-black snap-start justify-end`}>
-      <div className="col-span-2 flex flex-col pl-12 pb-12 justify-center">
+    <section className={`p-10 grid gap-10 md:grid-cols-2 lg:grid-cols-5 md:h-screen bg-black md:snap-start justify-center md:justify-end`}>
+      <div className="lg:col-span-2 flex flex-col p-5 md:pl-12 md:pb-12 justify-center">
         <StaticImage
           src="../images/sergiotbh_avatar.jpeg"
           width={200}
@@ -37,36 +37,36 @@ const IntroSection = () => {
           layout="fixed"
           className="rounded-full self-center"
         />
-        <h1 className="text-white text-6xl font-serif my-8">Hola!<br/> I'm Sergio,<br/>web developer</h1>
+        <h1 className="text-white text-4xl lg:text-6xl font-serif my-8">Hola!<br/> I'm Sergio,<br/>web developer</h1>
         <p className="text-primary-500 font-serif">
           // Frontend · React · React Native <br/>
           // Based in Guadalajara, México
         </p>
       </div>
-      <div className="relative col-span-3 h-full flex justify-center items-center">
+      <div className="relative lg:col-span-3 h-full flex justify-center items-center">
         <ul className="grid grid-cols-2 gap-12">
           <li className="bg-white p-5 rounded-3xl flex justify-center shadow-primary-500 shadow-lg hover:shadow-primary-500 hover:shadow-2xl">
             <img
               src={JsLogo}
-              className="w-36"
+              className="md:w-36"
             />
           </li>
           <li className="bg-white p-5 rounded-3xl flex justify-center shadow-primary-500 shadow-lg hover:shadow-primary-500 hover:shadow-2xl">
             <img
               src={GatsbyLogo}
-              className="w-36"
+              className="md:w-36"
             />
           </li>
           <li className="bg-white p-5 rounded-3xl flex justify-center shadow-primary-500 shadow-lg hover:shadow-primary-500 hover:shadow-2xl">
             <img
               src={ReactLogo}
-              className="w-36"
+              className="md:w-36"
             />
           </li>
           <li className="bg-white p-5 rounded-3xl flex justify-center shadow-primary-500 shadow-lg hover:shadow-primary-500 hover:shadow-2xl">
             <img
               src={TailwindLogo}
-              className="w-36"
+              className="md:w-36"
             />
           </li>
         </ul>
